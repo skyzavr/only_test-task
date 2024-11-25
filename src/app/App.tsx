0ae9e5db0ebbfdfde1c1c4bb90aa7c94555b4170
nino <dankozero111@gmail.com>
2024-11-25 17:44:36 +0300

    [feat]: add main component. that display widgets of its && handles current section position

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,44 @@
+import { useState } from 'react';
+
+import { SectionSlider } from '@widgets/sectionContentSlider';
+import { MainScreen } from '@widgets/mainScreen';
+import { SwipeSection } from '@features/swipeSection';
+
+import { data } from './lib/data';
+import './style.scss';
+
 export const App = () => {
-  return <h1>Hello</h1>;
+  const [currentValue, setCurrentValue] = useState(0);
+  const { listOfData } = data[currentValue];
+
+  const handleNext = () => {
+    if (currentValue >= data.length - 1) return;
+    setCurrentValue((prev) => prev + 1);
+  };
+  const handlePrev = () => {
+    if (currentValue === 0) return;
+    setCurrentValue((prev) => prev - 1);
+  };
+  const swiperSectionBtns = (
+    <SwipeSection
+      handleNext={handleNext}
+      handlePrev={handlePrev}
+      currentValue={currentValue}
+      total={data.length}
+    />
+  );
+  return (
+    <main>
+      <section className="wrapper">
+        <div className="verticalLine"></div>
+        <MainScreen
+          currentValue={currentValue}
+          setCurrentValue={setCurrentValue}
+        >
+          {swiperSectionBtns}
+        </MainScreen>
+        <SectionSlider data={listOfData}>{swiperSectionBtns}</SectionSlider>
+      </section>
+    </main>
+  );
 };
